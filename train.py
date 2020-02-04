2870311de7c6e66dafb56f48a52efe5afdfc3c3c
Drazen Dodik <drazend@live.fi>
2020-02-04 13:46:43 +0200

    Added TF2 sample code and outputted the model to Valohai outputs

--- train.py
+++ train.py
@@ -1,1 +1,31 @@
-print('hello world!')
+import tensorflow as tf
+import os
+
+# Get the output path from the Valohai machines environment variables
+output_path = os.getenv('VH_OUTPUTS_DIR')
+
+# Load and prepare the MNIST dataset. Convert the samples from integers to floating-point numbers:
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Build the tf.keras.Sequential model by stacking layers.
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
+
+# Choose Adam as the optimizer and choose a loss function for training:
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+
+model.evaluate(x_test,  y_test, verbose=2)
+
+# Save our file to that directory as model.h5
+model.save(os.path.join(output_path, 'model.h5'))
